6d7f64fdfcd7af4490f3612a20ebca49e53f01f1
aitehs <turetskiy5002@gmail.com>
2021-07-24 19:22:48 +0300

    refactured code for simple operations

--- calculator.py
+++ calculator.py
@@ -1,39 +1,20 @@
-#VARS AND LISTS
-global operation 
-global status
-operations = ["+","plus","-","minus", "/", "divide","*","multiply"]
-problem = []
-
-#FUNCS
-def ch_op():
+operations = ["+","-","/","*"]
+def number():
     while True:
-        operation = input("Choose operation:")
-        if operation == "=":
-            return 1
-            
-        elif operation in operations:
-            problem.append(operation)
-            print(problem)
-            return 0
-            
+        try:
+            number = int(input("Number:"))
+            return number
+        except:
+            continue
+def operation():
+     while True:
+
+        operation = input("Operation:")
+        if operation in operations:
+            return operation
         else:
             continue
-#input numbers and choose operation
-
-while True:
-    
-    try:
-        number = int(input("Number:"))
-        problem.append(str(number))
-        print(problem)
-        if(ch_op()):
-            break
-            
-       
-            
         
-    except:
-        continue
-#calculations themselves
-
-print(problem + "ok")
+while True:
+    print(eval(str(number()) + operation() + str(number())))
+        
